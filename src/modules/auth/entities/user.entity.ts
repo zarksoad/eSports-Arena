9e2c000efa714f19c11eb0bc5ad843a9877ab058
Hernan <110841804+zarksoad@users.noreply.github.com>
2024-10-07 14:48:02 -0500

    feat: added role entity

--- src/modules/auth/entities/user.entity.ts
+++ src/modules/auth/entities/user.entity.ts
@@ -1,4 +1,11 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Role } from './role.entity';
 
 @Entity('users')
 export class User {
@@ -10,4 +17,11 @@ export class User {
 
   @Column({ type: 'varchar' })
   password: string;
+
+  @Column({ type: 'integer' })
+  roleId: number;
+
+  @ManyToOne(() => Role, (role) => role.users)
+  @JoinColumn({ name: 'roleId' })
+  role: Role;
 }
