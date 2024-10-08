d565af8e1b5398e244f736ad50d3ad0c72489bc8
Hernan <110841804+zarksoad@users.noreply.github.com>
2024-10-07 21:46:16 -0500

    docs: added swagger

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,47 +1,31 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginDto } from './dto/login-user.dto';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
+@ApiTags('auth') // Grouping endpoints under 'auth'
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
+  @ApiOperation({ summary: 'Register a new user' }) // Operation summary for Swagger
+  @ApiResponse({
+    status: 201,
+    description: 'The user has been successfully created.',
+  }) // Response for successful creation
+  @ApiResponse({ status: 400, description: 'Bad Request.' }) // Response for bad request
   create(@Body() createAuthDto: CreateUserDto) {
     return this.authService.create(createAuthDto);
   }
+
   @Post('login')
+  @ApiOperation({ summary: 'Log in a user' }) // Operation summary for Swagger
+  @ApiResponse({ status: 200, description: 'Successfully logged in.' }) // Response for successful login
+  @ApiResponse({ status: 401, description: 'Unauthorized.' }) // Response for unauthorized
   logIn(@Body() loginDto: LoginDto) {
     return this.authService.logIn(loginDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.authService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
   }
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -17,20 +17,4 @@ export class AuthService {
   ): Promise<{ message: string; accessToken: string }> {
     return await this.userService.logIn(loginDto);
   }
-
-  findAll() {
-    return `This action returns all auth`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
-  }
 }
